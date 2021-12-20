20d324dc048c5c33e7d6acc5c99e947d6459d0f1
Kalamiyah <90818603+Kalamiyah@users.noreply.github.com>
2021-12-20 22:59:44 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 
       <div class="wrapper"><i class="bi bi-heart-fill text-danger"></i><b>Created By Imro'atul Kalamiyah(Nilam)</b></div>
 	   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css" />
-	<link rel="stylesheet" href="stylesheet.css" />
+	
 	</body>
 </html>
